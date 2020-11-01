0770bd30433831ce6183b7eb37ec75609d136d96
Finn Pelzer <finn@pelzers.com>
2020-11-01 23:38:46 +0100

    Initial Commit

---
+++ Assets/Scripts/SpawnNPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNPC : MonoBehaviour
+{
+    public int npcCount;
+    private List<NPC> npcList = new List<NPC>();
+    
+    void Start()
+    {
+        for (int i = 0; i < npcCount; i++)
+        {
+            npcList.Add(new NPC());
+        }
+    }
+}
